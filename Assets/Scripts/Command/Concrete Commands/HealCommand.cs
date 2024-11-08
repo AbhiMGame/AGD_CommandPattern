001c8c60e7ce14252b05b9d285e6adf07173f112
MoonJSonic <abhijeet.munje@gmail.com>
2024-11-08 13:06:56 +0530

    Created Concrete Commands
    Created concrete commands for the following unit actions and implemented all abstract methods in  UnitCommand class.
    Attack
    Heal
    Cleanse
    Attack Stance
    Meditate
    Berserk Attack
    Third Eye

---
+++ Assets/Scripts/Command/Concrete Commands/HealCommand.cs
@@ -0,0 +1,20 @@
+﻿using Command.Main;
+using Command.Actions;
+
+namespace Command.Commands
+{
+    public class HealCommand : UnitCommand
+    {
+        private bool willHitTarget;
+
+        public HealCommand(CommandData commandData)
+        {
+            this.commandData = commandData;
+            willHitTarget = WillHitTarget();
+        }
+
+        public override void Execute() => GameService.Instance.ActionService.GetActionByType(CommandType.Heal).PerformAction(actorUnit, targetUnit, willHitTarget);
+
+        public override bool WillHitTarget() => true;
+    }
+}
